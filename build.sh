be381c394c232b3d17bdbc9f519774ae5b08f20b
Tom Riley <tom@smallroomsoftware.com>
2021-05-18 20:28:05 +0100

    added 'yarn build' to build.sh script

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ export CC=../wasi-sdk-12.0/bin/clang\ --sysroot=../wasi-sysroot
 $CC -nostartfiles -Wl,--no-entry example.c -o dist/original.wasm
 # with polyfills
 $CC -nostartfiles -Wl,--no-entry example.c i64_polyfill.c -o dist/polyfilled.wasm
+
+# build js
+yarn build
